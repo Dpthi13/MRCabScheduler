8cf0afb061579636c3e6fb03f18fa8fc5e8473fe
Deepthi <dpthi132@gmail.com>
2025-02-12 18:44:18 +0530

    added databaseService, JWT, Dapper classes

---
+++ Cab.Infrastructure/DomainEntities/UserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cab.Infrastructure.DomainEntities
+{
+    public class UserInfo
+    {
+        public string EmpId {  get; set; }
+        public string EmpName {  get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string AccessToken {  get; set; }
+        public string RoleName {  get; set; }
+    }
+}
---
+++ Cab.Service/UserManagementService.cs
@@ -0,0 +1,39 @@
+﻿using Cab.Infrastructure.DomainEntities;
+using Cab.Infrastructure.Interfaces;
+using Cab.Infrastructure.Constants;
+using UserInfo = Cab.Infrastructure.DomainEntities.UserInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cab.Service
+{
+    public class UserManagementService : IUserManagementService
+    {
+       private readonly ITokenService _tokenService;
+        private readonly IDatabaseService _databaseService;
+        private Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+        public UserManagementService(ITokenService tokenService, IDatabaseService databaseService)
+        {
+            _tokenService = tokenService;
+            _databaseService = databaseService;
+        }
+
+        public async Task<UserInfo> GetUserInformationAsync(string empId)
+        {
+            if (parameters.Count > 0) parameters.Clear();
+            parameters.Add("empId", empId);
+            var userInfo = await _databaseService.ExecuteStoredProcAsync<UserInfo>(StoredProcedures.GetUserInformation, parameters);
+
+            if (userInfo == null)
+                return null;
+
+            userInfo.EmpId = empId;
+            userInfo.AccessToken = _tokenService.CreateToken(userInfo);
+            return userInfo;
+        }
+    }
+}
